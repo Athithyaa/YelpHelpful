04beb9dd036ae51cda5aff3a4ffde526a3216f57
athithyaa <atpa2251@colorado.edu>
2017-04-30 16:27:30 -0600

    adding tokenizer for review text

--- Amazon/rnn_lstm.py
+++ Amazon/rnn_lstm.py
@@ -6,6 +6,7 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
+from keras.preprocessing.text import Tokenizer
 from sklearn.model_selection import train_test_split
 
 numpy.random.seed(7)
@@ -20,8 +21,15 @@ with open('../reviews_data.csv', 'rb+') as inp_file:
     [X_train, X_test, y_train, y_test] = train_test_split(X[1:40000], y[1:40000], test_size=0.20)
     frqt_words = 8000
     review_len = 150
+
+    tok = Tokenizer(num_words=frqt_words, lower=True, split=" ")
+    tok.fit_on_texts(X_train)
+    X_train = tok.texts_to_sequences(X_train)
+    X_test = tok.texts_to_sequences(X_test)
+
     X_train = sequence.pad_sequences(X_train, maxlen=review_len)
     X_test = sequence.pad_sequences(X_test, maxlen=review_len)
+
     word_embeddings_length = 200
     model = Sequential()
     model.add(Embedding(frqt_words, word_embeddings_length, input_length=review_len))
@@ -29,7 +37,7 @@ with open('../reviews_data.csv', 'rb+') as inp_file:
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     print(model.summary())
-    model.fit(X_train, y_train, validation_data=(X_test, y_test), epoch=3, batch_size=128)
+    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3, batch_size=128)
 
 
 
